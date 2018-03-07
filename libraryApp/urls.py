22612547da36558bf723cf7421a80b9d0d395ec4
Mino <eng.m.sida@gmail.com>
2018-03-08 01:08:13 +0200

    adding sign up

--- libraryApp/urls.py
+++ libraryApp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .libraryApp import views as core_views
 
 from . import views
 app_name = 'libraryApp'
@@ -6,5 +7,6 @@ app_name = 'libraryApp'
 urlpatterns = [
 	path('',views.listWriters,name='writers'),
     path('d/<int:writer_id>',views.writerDetails,name='detail'),
+	url(r'^signup/$', core_views.signup, name='signup')
 
 ]
--- libraryApp/views.py
+++ libraryApp/views.py
@@ -10,3 +10,18 @@ def listWriters(request):
 def writerDetails(request,writer_id):
     writerDetails = writer.objects.get(pk=writer_id)
     return render(request,'writerDetails.html',{'details':writerDetails})
+
+    #signup
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
