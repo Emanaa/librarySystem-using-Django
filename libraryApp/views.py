9d0da02e26a996fe701231924b3d35f67bcb4d85
RaniaRefaat <rororefaat@gmail.com>
2018-03-07 19:53:24 +0200

    modifing the writer

--- libraryApp/views.py
+++ libraryApp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import Writer,Book
+from .models import writer,book
 
 # Create your views here.
 
 def listWriters(request):
-    writerList = Writer.objects.all()
+    writerList = writer.objects.all()
     return render(request, 'writerList.html',{'writer_list': writerList})
 
 def writerDetails(request,writer_id):
-    writerDetails = Writer.objects.get(pk=writer_id)
+    writerDetails = writer.objects.get(pk=writer_id)
     return render(request,'writerDetails.html',{'details':writerDetails})
