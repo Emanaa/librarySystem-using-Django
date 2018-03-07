d1734be578b17b8a42adc0d5ca36b14a1a185a24
samar <samarnasef80@gmail.com>
2018-03-07 06:29:42 +0200

    create view to get writer info. and send them to details.html page

--- libraryApp/views.py
+++ libraryApp/views.py
@@ -6,3 +6,7 @@ from .models import Writer,Book
 def listWriters(request):
     writerList = Writer.objects.all()
     return render(request, 'writerList.html',{'writer_list': writerList})
+
+def writerDetails(request,writer_id):
+    writerDetails = Writer.objects.get(pk=writer_id)
+    return render(request,'writerDetails.html',{'details':writerDetails})
