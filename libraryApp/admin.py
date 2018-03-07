1e6e3c234b171b8c4681e18aad1fbe525c450d93
Emanaa <emanazzam371@gmail.com>
2018-03-07 10:57:39 +0200

    add admin

--- libraryApp/admin.py
+++ libraryApp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import Writer, Book,Country
 # Register your models here.
+
+
+
+admin.site.register(Country)
+
+
+
+@admin.register(Writer)
+class Writerdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death','contact','books','bio')
+    fields = ['first_name', 'last_name','date_of_birth', 'date_of_death','contact','books','bio']
+
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'publishedAt','country','writer')
+
+
+
+
