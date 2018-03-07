90ad4843de5888714fd1d31a67881eb24618b861
samar <samarnasef80@gmail.com>
2018-03-07 06:20:25 +0200

    create view to get all writers and send this data to html page

--- libraryApp/views.py
+++ libraryApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Writer,Book
 
 # Create your views here.
+
+def listWriters(request):
+    writerList = Writer.objects.all()
+    return render(request, 'writerList.html',{'writer_list': writerList})
