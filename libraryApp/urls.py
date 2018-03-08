57eccef8f1d8d9ac7c04032c037334f671839784
Mino <eng.m.sida@gmail.com>
2018-03-08 15:39:42 +0200

    adding login & logout

--- libraryApp/urls.py
+++ libraryApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from .libraryApp import views as core_views
-from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 from . import views
 app_name = 'libraryApp'
@@ -9,6 +12,6 @@ urlpatterns = [
 	path('',views.listWriters,name='writers'),
     path('d/<int:writer_id>',views.writerDetails,name='detail'),
 	url(r'^signup/$', core_views.signup, name='signup'),
-	(r'^login/$','django.contrib.auth.views.login', {'template_name': 'login.html'}),
-	(r'^logout/$','django.contrib.auth.views.logout', {'next_page': '/login/'})
+	url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
 ]
