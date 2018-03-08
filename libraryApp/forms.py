fa750b6c760320473261fadf3bf83e39de4a3ce0
Mino <eng.m.sida@gmail.com>
2018-03-08 15:01:59 +0200

    adding login url

--- libraryApp/forms.py
+++ libraryApp/forms.py
@@ -8,19 +8,19 @@ class SignUpForm(UserCreationForm):
         def __init__(self, arg):
             super(SignUpForm, self).__init__()
             self.arg = arg
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    users_fname = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    users_lname = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    user_email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
         model = User
         fields = (
-        'username',
-        'first_name',
-        'last_name',
-        'email',
-        'password1',
-        'password2'
+        'users_uname',
+        'users_fname',
+        'users_lname',
+        'user_email',
+        'users_password',
+        'users_password2'
         )
 def save(self,commit=true):
     user = super(SignUpForm,self).save(commit=False)
--- libraryApp/urls.py
+++ libraryApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .libraryApp import views as core_views
+from django.contrib.auth.views import login
 
 from . import views
 app_name = 'libraryApp'
@@ -7,6 +8,7 @@ app_name = 'libraryApp'
 urlpatterns = [
 	path('',views.listWriters,name='writers'),
     path('d/<int:writer_id>',views.writerDetails,name='detail'),
-	url(r'^signup/$', core_views.signup, name='signup')
-
+	url(r'^signup/$', core_views.signup, name='signup'),
+	(r'^login/$','django.contrib.auth.views.login', {'template_name': 'login.html'}),
+	(r'^logout/$','django.contrib.auth.views.logout', {'next_page': '/login/'})
 ]
